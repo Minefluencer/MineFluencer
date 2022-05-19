94e8461d4b48ec0555b1442592f4a49ffb06879e
Jihoo <wlgn829@kakao.com>
2022-05-19 16:37:27 +0900

    새로운파일

---
+++ code_back/main.java
@@ -0,0 +1,12 @@
+//Java
+
+class hello {
+    public static void main(String[] args) {
+        int i = 10;
+        int a = 12;
+
+        if (i != a) {
+            System.out.println("오류");
+        }
+    }
+}
