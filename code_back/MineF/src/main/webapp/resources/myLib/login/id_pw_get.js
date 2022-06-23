231f1bc50677b2dc35365a2d3a655e6b155d2818
Choisungrim <87526812+Choisungrim@users.noreply.github.com>
2022-06-23 17:23:06 +0900

    Delete code_back/MineF directory

--- code_back/MineF/src/main/webapp/resources/myLib/login/id_pw_get.js
+++
@@ -1,9 +0,0 @@
-$('.btn_text > p').click(function () {
-	let tabId = $(this).attr('data-tab');
-
-	$('.btn_text > p').removeClass('active');
-	$('.content').removeClass('active');
-
-	$(this).addClass('active');
-	$('#' + tabId).addClass('active');
-});
